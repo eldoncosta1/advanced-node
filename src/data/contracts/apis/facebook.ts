45e54d16c880e7bd055e0caf937dd81d84dc990e
Eldon Costa <eldoncosta1@gmail.com>
2022-07-05 17:56:33 -0300

    ensure Service calls LoadUserAccountRepo when LoadFacebookUserApi returns data

--- src/data/contracts/apis/facebook.ts
+++ src/data/contracts/apis/facebook.ts
@@ -9,5 +9,11 @@ export namespace LoadFacebookUserApi {
     token: string
   }
 
-  export type Result = undefined
+  export type Result =
+    | undefined
+    | {
+        name: string
+        email: string
+        facebookId: string
+      }
 }
