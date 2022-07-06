63cebdb24949d66bf18769ba5f88ed7eb1fe0a2b
Eldon Costa <eldoncosta1@gmail.com>
2022-07-06 15:49:55 -0300

    move SUT dependencies creation to beforeAll

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -20,9 +20,10 @@ describe('FacebookAuthenticationService', () => {
     LoadUserAccountRepository & SaveFacebookAccountRepository
   >
   let sut: FacebookAuthenticationService
-  const token = 'any_token'
+  let token: string
 
-  beforeEach(() => {
+  beforeAll(() => {
+    token = 'any_token'
     facebookApi = mock()
     facebookApi.loadUser.mockResolvedValue({
       name: 'any_fb_name',
@@ -34,6 +35,10 @@ describe('FacebookAuthenticationService', () => {
     userAccountRepo.saveWithFacebook.mockResolvedValue({ id: 'any_account_id' })
     crypto = mock()
     crypto.generateToken.mockResolvedValue('any_generated_token')
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
     sut = new FacebookAuthenticationService(
       facebookApi,
       userAccountRepo,
