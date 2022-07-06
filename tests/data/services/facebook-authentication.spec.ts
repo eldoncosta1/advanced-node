e64eb1f18e034c0e5d3c9604835d1fb1bbffd2ac
Eldon Costa <eldoncosta1@gmail.com>
2022-07-06 15:26:44 -0300

    ensure Service calls TokenGeneratior with correct expiration

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -6,7 +6,7 @@ import {
 } from '@/data/contracts/repos'
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
-import { FacebookAccount } from '@/domain/models'
+import { AccessToken, FacebookAccount } from '@/domain/models'
 
 // import { mocked } from 'ts-jest/utils'
 import { mock, MockProxy } from 'jest-mock-extended'
@@ -83,7 +83,10 @@ describe('FacebookAuthenticationService', () => {
   it('should call TokenGenerator with correct params', async () => {
     await sut.perform({ token })
 
-    expect(crypto.generateToken).toHaveBeenCalledWith({ key: 'any_account_id' })
+    expect(crypto.generateToken).toHaveBeenCalledWith({
+      key: 'any_account_id',
+      expirationInMs: AccessToken.expirationInMs
+    })
     expect(crypto.generateToken).toHaveBeenCalledTimes(1)
   })
 })
